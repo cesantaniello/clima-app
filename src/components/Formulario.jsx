ed2ba29c45952775b708baf9b346a980d8c95a1b
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-12 15:01:16 +0200

    Read entries from Formulario and validation

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,29 +1,55 @@
+import { useState } from "react";
+import useClima from "../hooks/useClima";
+
 const Formulario = () => {
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda, consultarClima } = useClima();
+  const { ciudad, pais } = busqueda;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Todos los campos son obligatorios");
+      return;
+    }
+
+    consultarClima(busqueda);
+  };
+
   return (
     <div className="contenedor">
-        <form>
-            <div className="campo">
-                <label htmlFor="ciudad">Ciudad: </label>
-                <input type="text" name="ciudad" id="ciudad" placeholder="Ej. Londres" />
-            </div>
-            <div className="campo">
-                <label htmlFor="pais">País: </label>
-                <select name="pais" id="pais">
-                    <option value="">-- Seleccione un país --</option>
-                    <option value="US">Estados Unidos</option>
-                    <option value="MX">México</option>
-                    <option value="AR">Argentina</option>
-                    <option value="CO">Colombia</option>
-                    <option value="CR">Costa Rica</option>
-                    <option value="ES">España</option>
-                    <option value="PE">Perú</option>
-                    <option value="VE">Venezuela</option>
-                </select>
-            </div>
-            <input type="submit" value="Consultar Clima" />            
-        </form>
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad: </label>
+          <input
+            type="text"
+            name="ciudad"
+            id="ciudad"
+            placeholder="Ej. Londres"
+            onChange={datosBusqueda}
+            value={ciudad}
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País: </label>
+          <select name="pais" id="pais" onChange={datosBusqueda} value={pais}>
+            <option value="">-- Seleccione un país --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+            <option value="VE">Venezuela</option>
+          </select>
+        </div>
+        <input type="submit" value="Consultar Clima" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
