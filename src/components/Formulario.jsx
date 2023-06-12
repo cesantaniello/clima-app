4b832c7aedb268c6eaf3625c1657aa4947619d27
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-12 14:21:06 +0200

    Create Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,28 @@
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+        <form>
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad: </label>
+                <input type="text" name="ciudad" id="ciudad" placeholder="Ej. Londres" />
+            </div>
+            <div className="campo">
+                <label htmlFor="pais">País: </label>
+                <select name="pais" id="pais">
+                    <option value="">-- Seleccione un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="VE">Venezuela</option>
+                </select>
+            </div>
+            <input type="submit" value="Consultar Clima" />            
+        </form>
+    </div>
   )
 }
 
