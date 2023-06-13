9e85a82fa76e48af8d0d54acf5b92109c08de974
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-13 12:13:38 +0200

    Integrate API

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,4 +1,6 @@
 import { useState, createContext } from "react";
+import axios from "axios";
+
 
 const ClimaContext = createContext();
 
@@ -8,6 +10,8 @@ const ClimaProvider = ({ children }) => {
     pais: "",
   });
 
+  const [resultado, setResultado] = useState({});
+
   const datosBusqueda = (e) => {
     setBusqueda({
       ...busqueda,
@@ -15,12 +19,28 @@ const ClimaProvider = ({ children }) => {
     });
   };
 
-  const consultarClima = datos => {
-    console.log(datos)
+  const consultarClima = async datos => {
+    try {
+      const { ciudad, pais } = datos;
+      const appId = import.meta.env.VITE_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+      const {data} = await axios(url);
+      console.log(data)
+      const { coord } = data;
+      const { lat, lon } = coord;
+      console.log(lat, lon);
+
+      const urlClima = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${appId}`;
+      const {data: dataClima} = await axios(urlClima);
+      setResultado(dataClima)
     }
+    catch (error) {
+      console.log(datos)
+    }
+  }
 
   return (
-    <ClimaContext.Provider value={{ busqueda, datosBusqueda, consultarClima }}>
+    <ClimaContext.Provider value={{ busqueda, datosBusqueda, consultarClima, resultado }}>
       {children}
     </ClimaContext.Provider>
   );
